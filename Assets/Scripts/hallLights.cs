c0fc5add1aeff3e1085707b0c6ca4f6db9e12c7f
tannereubanks759 <112025050+tannereubanks759@users.noreply.github.com>
2023-11-30 17:18:00 -0500

    GFFGB
    FFB

---
+++ Assets/Scripts/hallLights.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hallLights : MonoBehaviour
+{
+    public Light light1;
+    public Light light2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
